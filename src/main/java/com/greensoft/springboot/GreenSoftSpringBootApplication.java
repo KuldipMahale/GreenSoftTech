ad48aa5756c6451757f63c6256e52f385523c444
aakksshhaayy4 <akhareakshay2@gmail.com>
2023-07-26 17:01:10 +0530

    initial commit

---
+++ src/main/java/com/greensoft/springboot/GreenSoftSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.greensoft.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenSoftSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenSoftSpringBootApplication.class, args);
+	}
+
+}
