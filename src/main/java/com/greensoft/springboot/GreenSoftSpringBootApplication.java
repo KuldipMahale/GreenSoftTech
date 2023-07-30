4b360bf4a293841cf6ed64decd006c50cd29a3df
gauravpawar409 <56555597+gauravpawar409@users.noreply.github.com>
2023-07-30 11:30:07 +0530

    Created Bean for ModelMap

--- src/main/java/com/greensoft/springboot/GreenSoftSpringBootApplication.java
+++ src/main/java/com/greensoft/springboot/GreenSoftSpringBootApplication.java
@@ -1,7 +1,9 @@
 package com.greensoft.springboot;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GreenSoftSpringBootApplication {
@@ -9,5 +11,8 @@ public class GreenSoftSpringBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GreenSoftSpringBootApplication.class, args);
 	}
+
+	@Bean
+	public ModelMapper model(){ return new ModelMapper();}
 
 }
